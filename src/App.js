37c41c7a980385abf5fc813851f321c36d23718a
Ronald Jabouin <rj336@cornell.edu>
2024-06-15 08:15:01 -0700

    added an onClick event to search. can fill in dep loc, arr loc, and date for search url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React,{ useState } from 'react';
 import './App.css';
 
 function App() {
-  const apiUrl = `https://get-me-home.onrender.com/flix/06-20-2024/new_york/ithaca`
   let today = new Date();
   let dd = String(today.getDate()).padStart(2, '0');
   let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -18,12 +17,37 @@ function App() {
   
   maxDate = maxYyyy + '-' + maxMm + '-' + maxDd;
 
+  const handleButtonClick = () => {
+    console.log('button clicked');
+    console.log(apiUrl);
+  };
+
+  const handleSelectChange = (setter) => (event) => {
+    setter(event.target.value);
+    if (setter === setDepartureDate) {
+      console.log(event.target.value);
+      const dateFromInput = event.target.value.split('-');
+      const year = dateFromInput[0];
+      const month = dateFromInput[1];
+      const day = dateFromInput[2];
+      const newYearString = month + '-' + day + '-' + year;
+      setDepartureDate(newYearString);
+    }
+  };
+
+  const [departureLocation, setDepartureLocation] = useState('new_york');
+  const [arrivalLocation, setArrivalLocation] = useState('ithaca');
+  const [departureDate, setDepartureDate] = useState(today);
+
+  const apiUrl = `https://get-me-home.onrender.com/flix/${departureDate}/${departureLocation}/${arrivalLocation}`
+
   return (
     <div className="app">
       <h1>GetMeHome</h1>
       <div className="loc_dropdown">
         <h3>Departure Location</h3>
-        <select>
+        {/* when I choose an option, I want to set the state of setDeparture to the option value */}
+        <select onChange={handleSelectChange(setDepartureLocation)}>
           <option value="new_york">NYC</option>
           <option value="ithaca">Ithaca, NY</option>
           <option value="jfk">JFK Airport</option>
@@ -41,7 +65,7 @@ function App() {
       </div>
       <div className="loc_dropdown">
         <h3>Arrival Location</h3>
-        <select>
+        <select onChange={handleSelectChange(setArrivalLocation)}>
           <option value="new_york">NYC</option>
           <option value="ithaca">Ithaca, NY</option>
           <option value="jfk">JFK Airport</option>
@@ -58,10 +82,14 @@ function App() {
         </select>
       </div>
       <div className="date_dropdown">
-        <h3>Date</h3>
-        <input type="date" min={today} max={maxDate}></input>
+        <h3>Departure Date</h3>
+        <input type="date" min={today} max={maxDate} onChange={handleSelectChange(setDepartureDate)} />
       </div>
-    </div>
+      {/* create a div called search that contains a button that says search */}
+      <div className="search">
+        <button onClick={handleButtonClick}>Search</button>       
+      </div>
+      </div>
   );
 }
 
