f7bd63dfa594dd3c387bb4875d97bc062b5cbdb5
Ronald Jabouin <rj336@cornell.edu>
2024-06-15 08:02:20 -0700

    added title, dropdowns, and date with restrictions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
 
 function App() {
+  const apiUrl = `https://get-me-home.onrender.com/flix/06-20-2024/new_york/ithaca`
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
+  
+  today = yyyy + '-' + mm + '-' + dd;
+  
+  let maxDate = new Date();
+  maxDate.setMonth(maxDate.getMonth() + 2);
+  let maxDd = String(maxDate.getDate()).padStart(2, '0');
+  let maxMm = String(maxDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let maxYyyy = maxDate.getFullYear();
+  
+  maxDate = maxYyyy + '-' + maxMm + '-' + maxDd;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ok <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>GetMeHome</h1>
+      <div className="loc_dropdown">
+        <h3>Departure Location</h3>
+        <select>
+          <option value="new_york">NYC</option>
+          <option value="ithaca">Ithaca, NY</option>
+          <option value="jfk">JFK Airport</option>
+          <option value="binghamton">Binghamton, NY</option>
+          <option value="rochester">Rochester, NY</option>
+          <option value="albany">Albany, NY</option>
+          <option value="buffalo">Buffalo, NY</option>
+          <option value="syracuse">Syracuse, NY</option>
+          <option value="syr_airport">SYR Airport</option>
+          <option value="newark">Newark, NJ</option>
+          <option value="philly">Philadelphia, PA</option>
+          <option value="baltimore">Baltimore, MD</option>
+          <option value="boston">Boston, MA</option>
+        </select>
+      </div>
+      <div className="loc_dropdown">
+        <h3>Arrival Location</h3>
+        <select>
+          <option value="new_york">NYC</option>
+          <option value="ithaca">Ithaca, NY</option>
+          <option value="jfk">JFK Airport</option>
+          <option value="binghamton">Binghamton, NY</option>
+          <option value="rochester">Rochester, NY</option>
+          <option value="albany">Albany, NY</option>
+          <option value="buffalo">Buffalo, NY</option>
+          <option value="syracuse">Syracuse, NY</option>
+          <option value="syr_airport">SYR Airport</option>
+          <option value="newark">Newark, NJ</option>
+          <option value="philly">Philadelphia, PA</option>
+          <option value="baltimore">Baltimore, MD</option>
+          <option value="boston">Boston, MA</option>
+        </select>
+      </div>
+      <div className="date_dropdown">
+        <h3>Date</h3>
+        <input type="date" min={today} max={maxDate}></input>
+      </div>
     </div>
   );
 }
