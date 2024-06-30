2ca7b8ceb8292876e77c8382ef8d75a0a51d6230
Chimdi <ce248@cornell.edu>
2024-06-30 19:44:26 -0400

    Add styles to program so program resembles mobile application counterpart

--- src/App.js
+++ src/App.js
@@ -40,10 +40,10 @@ function App() {
   const [trips, setTrips] = useState([]);
   const [loading, setLoading] = useState(false);
   const [dataFetched, setDataFetched] = useState(false);
-  const [departureTime, setDepartureTime] = useState(true); // State for departure time button
-  const [arrivalTime, setArrivalTime] = useState(true); // State for departure time button
-  const [maxPrice, setMaxPrice] = useState(true);
-  const [rangeValue, setRangeValue] = useState(1);
+  const [departureTime, setDepartureTime] = useState(false); // State for departure time button
+  const [arrivalTime, setArrivalTime] = useState(false); // State for departure time button
+  const [maxPrice, setMaxPrice] = useState(false); // State for max price button
+  const [rangeValue, setRangeValue] = useState(100);
 
   const apiUrl = `https://get-me-home.onrender.com/flix/${departureDate}/${departureLocation}/${arrivalLocation}`;
 
@@ -150,96 +150,103 @@ function App() {
 
   return (
     <div className="app">
-      <h1 className='gmh'>GetMeHome</h1>
-      <div className="row">
-        <div className="loc_dropdown">
-          <h3>Departure</h3>
-          <select value={departureLocation} onChange={handleSelectChange(setDepartureLocation)} disabled={loading}>
-            <option value="" disabled>Select a location</option>
-            {filterOptions(arrivalLocation).map(option => (
-              <option key={option} value={options[option]}>{option}</option>
-            ))}
-          </select>
-        </div>
-        <div className="loc_dropdown">
-          <h3>Arrival</h3>
-          <select value={arrivalLocation} onChange={handleSelectChange(setArrivalLocation)} disabled={loading}>
-            <option value="" disabled>Select a location</option>
-            {filterOptions(departureLocation).map(option => (
-              <option key={option} value={options[option]}>{option}</option>
-            ))}
-          </select>
-        </div>
-        <div className="date_dropdown">
-          <h3>Date</h3>
-          <input type="date" min={today} max={maxDate} onChange={handleSelectChange(setDepartureDate)} disabled={loading}/>
-        </div>
-        <div>
-          <button onClick={switchLocations} disabled={loading}>Switch</button>
-        </div>
-      </div>
-
-      <div>
-        <button style={{width: "fit-content"}} onClick={() => setDepartureTime(!departureTime)}>Departure Time</button>
-      </div>
-
-      {
-        departureTime && (
+      <div style={{width: "100vw", height: "100vh", backgroundColor: "black", padding: "0"}}>
+        <h1 className='gmh' style={{color: "white"}}>GetMeHome</h1>
+        <div className="row">
+          <div className="loc_dropdown">
+            <h3 style={{color: "white"}}>Departure</h3>
+            <select className="primary_field" value={departureLocation} onChange={handleSelectChange(setDepartureLocation)} disabled={loading}>
+              <option value="" disabled>Select a location</option>
+              {filterOptions(arrivalLocation).map(option => (
+                <option key={option} value={options[option]}>{option}</option>
+              ))}
+            </select>
+          </div>
+          <div className="loc_dropdown">
+            <h3 style={{color: "white"}}>Arrival</h3>
+            <select className="primary_field" value={arrivalLocation} onChange={handleSelectChange(setArrivalLocation)} disabled={loading}>
+              <option value="" disabled>Select a location</option>
+              {filterOptions(departureLocation).map(option => (
+                <option key={option} value={options[option]}>{option}</option>
+              ))}
+            </select>
+          </div>
+          <div className="date_dropdown">
+            <h3 style={{color: "white"}}>Date</h3>
+            <input className="primary_field" type="date" min={today} max={maxDate} onChange={handleSelectChange(setDepartureDate)} disabled={loading}/>
+          </div>
           <div>
-            Earliest Departure Time:
-            <input type="time" step="3600"></input>
+            <button className="primary_field" onClick={switchLocations} disabled={loading}>Switch</button>
           </div>
-        )
-      }
-
-      <div>
-        <button style={{width: "fit-content"}} onClick={() => setArrivalTime(!arrivalTime)}>Arrival Time</button>
-      </div>
-
-      {
-        arrivalTime && (
-          <div>
-            Latest Arrival Time:
-            <input type="time" step="3600"></input>
-          </div>
-        )
-      }
-      <div style={{display:"inline-flex", alignItems: "center"}}>
-        <button style={{width: "fit-content"}} onClick={() => setMaxPrice(!maxPrice)}>Max Price</button>
-        {
-        maxPrice && (
-          <div style={{}}>
-            <input type="range" min="1" max="400" step="1" onChange={changeValue} value={rangeValue}></input>
-            ${rangeValue}.00
-          </div>
-        )
-      }
-      </div>
-
-      
-
-      <div className="search">
-        <button onClick={handleButtonClick} disabled={loading}>Search</button>
-      </div>
-
-      {loading ? (
-        <div className="loading-indicator">Loading...</div>
-      ) : trips.length === 0 ? (
-        dataFetched ? (
-          <div style={{textAlign: "center", fontWeight: "bold", marginTop: "2%"}}>
-          There are no trips from {getKey(departureLocation)} to {getKey(arrivalLocation)} on the date {departureDate}.
         </div>
+
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly", marginLeft: "1%", marginRight: "2%"}}>
+          
+          
+          <div style={{margin: "1em auto"}}>
+            <button className="secondary_field" style={{width: "fit-content"}} onClick={() => setDepartureTime(!departureTime)}>Departure Time</button>
+                {
+            departureTime && (
+              <div style={{color: "white"}}>
+                Earliest Departure Time:
+                <input style={{color: "white"}} type="time" step="3600" ></input>
+              </div>
+            )
+          }       
+          </div>
+
+          <div style={{margin: "1em auto"}}>
+            <button className="secondary_field" style={{width: "fit-content"}} onClick={() => setArrivalTime(!arrivalTime)}>Arrival Time</button>
+            {
+            arrivalTime && (
+              <div style={{color: "white"}}>
+                Latest Arrival Time:
+                <input style={{color: "white"}}type="time" step="3600"></input>
+              </div>
+            )
+          }
+          </div>
+          
+          {/*style={{display:"inline-flex", alignItems: "center"}}*/}
+          <div style={{margin: "1em auto"}}>
+            <button className="secondary_field" style={{width: "fit-content"}} onClick={() => setMaxPrice(!maxPrice)}>Max Price</button>
+            {
+            maxPrice && (
+              <div style={{color: "white"}}>
+                Max Price
+                <input type="range" min="1" max="400" step="1" onChange={changeValue} value={rangeValue}></input>
+                ${rangeValue}.00
+              </div>
+            )
+          }
+          </div>
+        </div>
+
+        
+
+        <div className="search">
+          <button className="primary_field"  style={{width: "100%"}} onClick={handleButtonClick} disabled={loading}>Search</button>
+        </div>
+
+        {loading ? (
+          <div className="loading-indicator">Loading...</div>
+        ) : trips.length === 0 ? (
+          dataFetched ? (
+            <div style={{color:"white", textAlign: "center", fontWeight: "bold", marginTop: "2%"}}>
+            There are no trips from {getKey(departureLocation)} to {getKey(arrivalLocation)} on the date {departureDate}.
+          </div>
+          ) : (
+            <div style={{color: "white", textAlign: "center", marginTop: "2%"}}>
+              <h3>Search For Trips</h3>
+              <p>Select a Date, Departure Location, and Destination</p>
+            </div>
+          )
         ) : (
-          <div style={{textAlign: "center", marginTop: "2%"}}>
-            <h3>Search For Trips</h3>
-            <p>Select a Date, Departure Location, and Destination</p>
+          <div className="trip">
+            <TripGrid trips={trips} dTime={departureTime} aTime={arrivalTime} mPrice={maxPrice} />
           </div>
-        )
-      ) : (
-        <div className="trip">
-          <TripGrid trips={trips} />
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
--- src/TripGrid.js
+++ src/TripGrid.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import TripRowView from './TripRowView';
 
-const TripGrid = ({ trips }) => {
+const TripGrid = ({ trips, dTime, aTime, mPrice}) => {
   return (
-    <div className="trip-grid">
+    <div className="trip-grid" style={{color: "white"}}>
       {trips.map((trip, index) => (
         <TripRowView key={index} trip={trip} />
       ))}
